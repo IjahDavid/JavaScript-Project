2256e757245c8199451051ba46c0a9a388ba2c00
IjahDavid <David.ijayp@gmail.com>
2024-11-26 16:31:44 +0100

    fetch added Product page Design-day_20

---
+++ Product page Design-day_20/app.js
@@ -0,0 +1,27 @@
+let productImg = document.getElementById("productImg");
+let btn = document.getElementsByClassName("btn");
+
+btn[0].onclick = function(){
+    productImg.src = "Img/BMW.png";
+
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[1].onclick = function(){
+    productImg.src = "Img/Car 2.png";
+
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[2].onclick = function(){
+    productImg.src = "Img/car.png";
+
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
